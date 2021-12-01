e2d117fb742e6540fde611e8e1b6e95ab616f714
amine-elghazali <amine.marzak10@gmail.com>
2021-12-01 01:13:30 +0100

    find user by id

--- src/main/java/CQRS/QueriesApi/controller/UserQueryController.java
+++ src/main/java/CQRS/QueriesApi/controller/UserQueryController.java
@@ -1,14 +1,19 @@
 package CQRS.QueriesApi.controller;
 
 import CQRS.CommandsApi.entity.UserEntity;
+import CQRS.QueriesApi.query.FindUserQuery;
 import CQRS.QueriesApi.query.GetUsersQuery;
+import org.axonframework.messaging.responsetypes.ResponseType;
 import org.axonframework.messaging.responsetypes.ResponseTypes;
 import org.axonframework.queryhandling.QueryGateway;
+import org.axonframework.queryhandling.QueryHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/query/")
@@ -27,5 +32,13 @@ public class UserQueryController {
         List<UserEntity> users = queryGateway.query(getUsersQuery, ResponseTypes.multipleInstancesOf(UserEntity.class)).join();
 
         return users;
+    }
+
+    @GetMapping("user/{id}")
+    public UserEntity getOneUser(@PathVariable("id") UUID id){
+
+        FindUserQuery findUserQuery = new FindUserQuery(id);
+        UserEntity user = queryGateway.query(findUserQuery,UserEntity.class).join();
+        return user;
     }
 }
--- src/main/java/CQRS/QueriesApi/service/UserQueryServiceImpl.java
+++ src/main/java/CQRS/QueriesApi/service/UserQueryServiceImpl.java
@@ -2,6 +2,7 @@ package CQRS.QueriesApi.service;
 
 import CQRS.CommandsApi.entity.UserEntity;
 import CQRS.CommandsApi.repository.UserRepo;
+import CQRS.QueriesApi.query.FindUserQuery;
 import CQRS.QueriesApi.query.GetUsersQuery;
 
 import org.axonframework.queryhandling.QueryHandler;
@@ -20,5 +21,10 @@ public class UserQueryServiceImpl {
     @QueryHandler
     public List<UserEntity> handle(GetUsersQuery getUsersQuery){
         return userRepo.findAll();
+    }
+
+    @QueryHandler
+    public UserEntity handle(FindUserQuery findUserQuery){
+        return userRepo.findById(findUserQuery.getId()).get();
     }
 }
